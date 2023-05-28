cdae340b38938ed486acc3906e9a662114ad78e0
CLA-GITHUB <christwealthla@gmail.com>
2023-05-28 11:54:28 +0100

    tweet component

--- components/tweets/index.tsx
+++ components/tweets/index.tsx
@@ -1,6 +1,9 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React, { FC } from "react";
+import { Entypo } from "@expo/vector-icons";
+
 import { TweetTypes } from "../../types";
+import IconButton from "./icon-buttons";
 
 type TweetProps = {
   tweet: TweetTypes;
@@ -15,8 +18,31 @@ const Tweet: FC<TweetProps> = ({ tweet }) => {
         style={styles.userImage}
       />
       <View style={styles.contentContainer}>
-        <Text style={styles.name}>{tweet.user.username}</Text>
+        <View style={styles.namesContainer}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>@{tweet.user.username} · 2h</Text>
+          <Entypo
+            name='dots-three-horizontal'
+            size={16}
+            color='gray'
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
         <Text style={styles.content}>{tweet.content}</Text>
+        {tweet.image && (
+          <Image
+            source={{ uri: tweet?.image }}
+            alt='profile image'
+            style={styles.image}
+          />
+        )}
+        <View style={styles.footer}>
+          <IconButton icon='comment' text={tweet.numberOfComments} />
+          <IconButton icon='retweet' text={tweet.numberOfRetweets} />
+          <IconButton icon='heart' text={tweet.numberOfLikes} />
+          <IconButton icon='chart' text={tweet.impressions || 0} />
+          <IconButton icon='share-apple' />
+        </View>
       </View>
     </View>
   );
@@ -37,12 +63,31 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     flex: 1,
   },
+  image: {
+    marginVertical: 5,
+    borderRadius: 15,
+    width: "100%",
+    aspectRatio: 19 / 9,
+  },
+  name: {
+    fontWeight: "600",
+  },
+  namesContainer: {
+    flexDirection: "row",
+  },
   userImage: {
     width: 50,
     height: 50,
     borderRadius: 50,
   },
-  name: {
-    fontWeight: "600",
+  username: {
+    color: "gray",
+    marginLeft: 5,
+  },
+
+  footer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    justifyContent: "space-between",
   },
 });
